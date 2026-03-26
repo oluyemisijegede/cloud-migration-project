79b72f50371058702e7cf3f92842898b367dd5b9
oluyemisijegede <oluyemisijegede@gmail.com>
2026-03-26 12:34:13 -0500

    Create main.tf

---
+++ docker_flask_app/main.tf
@@ -0,0 +1,14 @@
+# Dockerized Flask Application
+
+## Overview
+Containerized a Python Flask application using Docker.
+
+## Features
+- Custom Dockerfile
+- Dependency management
+- Port exposure
+- Docker Hub integration
+
+## Commands
+docker build -t app .
+docker run -p 3000:3000 app
