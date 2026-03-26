638cc63a4a7d4c3341a41a5a4e29c109733ca76c
oluyemisijegede <oluyemisijegede@gmail.com>
2026-03-26 12:32:35 -0500

    Create main.tf

---
+++ kubernetes_deployments/main.tf
@@ -0,0 +1,14 @@
+# Kubernetes Deployments (EKS)
+
+## Overview
+Deployed containerized applications on Kubernetes using EKS.
+
+## Features
+- Pods, Deployments, Services
+- Rolling updates
+- Auto-scaling
+- Self-healing
+
+## Tools
+- kubectl
+- eksctl
